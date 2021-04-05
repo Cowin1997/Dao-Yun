5330c84c8172879049428445add87d970c987ed3
Cowin1997 <36791203+Cowin1997@users.noreply.github.com>
2021-04-05 22:36:39 +0800

    添加数据库模型和sql脚本

---
+++ Web End/src/main/java/cn/edu/fzu/interceptor/AuthorityInterceptor.java
@@ -0,0 +1,57 @@
+package cn.edu.fzu.interceptor;
+
+import cn.edu.fzu.entity.User;
+import org.apache.ibatis.plugin.Intercepts;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class AuthorityInterceptor implements HandlerInterceptor {
+    private static final Set<String> NOT_INTERCEPT_URI = new HashSet<>();//不拦截的URI
+
+    static {
+        NOT_INTERCEPT_URI.add("/user/login");
+    }
+
+
+    // 进入Handler方法之前执行。可以用于身份认证、身份授权。
+    // 比如如果认证没有通过表示用户没有登陆，需要此方法拦截不再往下执行（return false），
+    // 否则就放行（return true）
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("AuthorityInterceptor：" + request.getRequestURI());
+        String uri = request.getRequestURI();
+        if (!NOT_INTERCEPT_URI.contains(uri)) {
+            System.out.println("不拦截" + uri);
+            return true;
+        }else {
+            System.out.println("拦截" + uri);
+            HttpSession session = request.getSession(false); //如果不存在则不会产生一个session,而是返回null
+            if(session==null) {
+                throw new RuntimeException("用户未登陆");
+            }
+            User user = (User) session.getAttribute("user");
+            if (user == null) {
+                throw new RuntimeException("用户未登陆");
+            }
+            return true;
+        }
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
